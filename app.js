12fac61941386e9f964ff55f4e5492b6da2d228c
Michelle Ross <miross629@gmail.com>
2022-11-14 14:46:03 -0800

    Edit comments

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ const dogListContainer = document.getElementById('dog-list-container');
 // on load
 // eslint-disable-next-line space-before-function-paren
 window.addEventListener('load', async () => {
-    // fetch all dogs
+    // fetch all dogs (from fetch-utils)
     const dogs = await getDogs();
 
-    // render and append all dog cards to the container
+    // render and append all dog cards to the container (from render-utils)
     for (let dog of dogs) {
         const dogEl = renderDogCard(dog);
         dogListContainer.append(dogEl);
